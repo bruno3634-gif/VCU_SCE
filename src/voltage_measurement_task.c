d91615cb36e7a5aedb0e2ab9847439ee1e38bcab
perdo1305 <pedroandre.sferreira@gmail.com>
2024-12-08 13:24:10 +0000

    queues tested and added voltage measurement functions

--- src/voltage_measurement_task.c
+++ src/voltage_measurement_task.c
@@ -28,6 +28,8 @@
 // *****************************************************************************
 
 #include "voltage_measurement_task.h"
+#include "definitions.h" 
+#include "toolchain_specifics.h"
 
 // *****************************************************************************
 // *****************************************************************************
@@ -52,7 +54,12 @@
 
 VOLTAGE_MEASUREMENT_TASK_DATA voltage_measurement_taskData;
 
+ 
+xSemaphoreHandle voltageMeasurementSemaphore; 
+ 
+__COHERENT uint16_t voltageMeasurementValue; 
 
+float MeasureVoltage(uint16_t);
 
 // *****************************************************************************
 // *****************************************************************************
@@ -61,7 +68,20 @@ VOLTAGE_MEASUREMENT_TASK_DATA voltage_measurement_taskData;
 // *****************************************************************************
 
 /* TODO:  Add any necessary callback functions.
+ * 
 */
+
+
+void ADCHS_CH8_Callback(ADCHS_CHANNEL_NUM channel, uintptr_t context) { 
+    static BaseType_t xHigherPriorityTaskWoken; 
+ 
+    xHigherPriorityTaskWoken = pdFALSE; 
+     
+    xSemaphoreGiveFromISR(voltageMeasurementSemaphore, &xHigherPriorityTaskWoken); 
+    if (xHigherPriorityTaskWoken == pdTRUE) { 
+        portYIELD(); 
+    } 
+} 
 
 // *****************************************************************************
 // *****************************************************************************
@@ -92,6 +112,13 @@ void VOLTAGE_MEASUREMENT_TASK_Initialize ( void )
 {
     /* Place the App state machine in its initial state. */
     voltage_measurement_taskData.state = VOLTAGE_MEASUREMENT_TASK_STATE_INIT;
+    
+    ADCHS_CallbackRegister(ADCHS_CH8, ADCHS_CH8_Callback, (uintptr_t)NULL);  // Voltage Measurement 
+    ADCHS_ChannelResultInterruptEnable(ADCHS_CH8); 
+    ADCHS_ChannelConversionStart(ADCHS_CH8); 
+ 
+    vSemaphoreCreateBinary(voltageMeasurementSemaphore); 
+    xSemaphoreTake(voltageMeasurementSemaphore, 0); 
 
 
 
@@ -131,7 +158,8 @@ void VOLTAGE_MEASUREMENT_TASK_Tasks ( void )
 
         case VOLTAGE_MEASUREMENT_TASK_STATE_SERVICE_TASKS:
         {
-
+            xSemaphoreTake(voltageMeasurementSemaphore, portMAX_DELAY); 
+            voltageMeasurementValue = MeasureVoltage(ADCHS_ChannelResultGet(ADCHS_CH8)); 
             break;
         }
 
@@ -145,6 +173,47 @@ void VOLTAGE_MEASUREMENT_TASK_Tasks ( void )
             break;
         }
     }
+}
+
+/// @brief Measure the voltage from a ADC channel
+/// @param channel  ADC channel to measure the voltage
+float MeasureVoltage(uint16_t bits) {
+    float PDM_Voltage;
+//    float LV_SOC;
+    PDM_Voltage = ((float)bits * 3.30 / 4095.000) / 0.1155;
+    //24.0 = 0% and 28.0 = 100%
+    //LV_SOC = (uint16_t)((PDM_Voltage - 24.0) * 1000 / 4.0);
+
+    if (PDM_Voltage >= 25) {
+        // set pin
+        //GPIO_RG9_LV_ON_Set();
+    } else if (PDM_Voltage < 25) {
+        static uint16_t previousMillis = 0;
+        uint16_t currentMillis = 0;
+        uint16_t interval = 0;
+
+        //currentMillis = millis();
+        interval = currentMillis - previousMillis;
+
+        if (PDM_Voltage < 25 && PDM_Voltage >= 24) {
+            if (interval >= 1000) {
+                //GPIO_RG9_LV_ON_Toggle();
+                previousMillis = currentMillis;
+            }
+        } else if (PDM_Voltage < 24 && PDM_Voltage >= 23) {
+            if (interval >= 500) {
+                //GPIO_RG9_LV_ON_Toggle();
+                previousMillis = currentMillis;
+            }
+        } else if (PDM_Voltage < 23) {
+            if (interval >= 150) {
+                //GPIO_RG9_LV_ON_Toggle();
+                previousMillis = currentMillis;
+            }
+        }
+    }
+
+    return PDM_Voltage;
 }
 
 
