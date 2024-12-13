fd5c63db02573ffcbd72d362be7e28cbd41394cb
perdo1305 <pedroandre.sferreira@gmail.com>
2024-12-13 20:05:29 +0000

    fix can?
    f

--- src/can_send_task.c
+++ src/can_send_task.c
@@ -27,13 +27,13 @@
 // *****************************************************************************
 // *****************************************************************************
 
-#include <stdio.h>
-
 #include "can_send_task.h"
-//#include "peripheral/canfd/plib_canfd1.h"
+
+#include <stdio.h>
+// #include "peripheral/canfd/plib_canfd1.h"
+#include "../SCE_VCU_FreeRTOS.X/queue_manager.h"
 #include "definitions.h"
 #include "queue.h"
-#include "../SCE_VCU_FreeRTOS.X/queue_manager.h"
 #include "semphr.h"
 // *****************************************************************************
 // *****************************************************************************
@@ -72,7 +72,7 @@ CANFD_MSG_RX_ATTRIBUTE msgAttr = CANFD_MSG_RX_DATA_FRAME;
 // *****************************************************************************
 
 /* TODO:  Add any necessary callback functions.
-*/
+ */
 
 // *****************************************************************************
 // *****************************************************************************
@@ -84,9 +84,7 @@ bool CanSend_task(uint32_t id, uint8_t length, uint8_t *buffer) {
 }
 
 /* TODO:  Add any necessary local functions.
-*/
-
-
+ */
 
 // *****************************************************************************
 // *****************************************************************************
@@ -102,12 +100,11 @@ bool CanSend_task(uint32_t id, uint8_t length, uint8_t *buffer) {
     See prototype in can_send_task.h.
  */
 
-void CAN_SEND_TASK_Initialize ( void )
-{
+void CAN_SEND_TASK_Initialize(void) {
     /* Place the App state machine in its initial state. */
     can_send_taskData.state = CAN_SEND_TASK_STATE_INIT;
-    
-    for(int i = 0; i<8;i++){
+
+    for (int i = 0; i < 8; i++) {
         message[i] = 0;
     }
 
@@ -115,9 +112,7 @@ void CAN_SEND_TASK_Initialize ( void )
      * parameters.
      */
     CAN1_Initialize();
-    
 }
-
 
 /******************************************************************************
   Function:
@@ -127,79 +122,64 @@ void CAN_SEND_TASK_Initialize ( void )
     See prototype in can_send_task.h.
  */
 
-void CAN_SEND_TASK_Tasks ( void )
-{
-
+void CAN_SEND_TASK_Tasks(void) {
     /* Check the application's current state. */
-    switch ( can_send_taskData.state )
-    {
+    switch (can_send_taskData.state) {
         /* Application's initial state. */
-        case CAN_SEND_TASK_STATE_INIT:
-        {
+        case CAN_SEND_TASK_STATE_INIT: {
             bool appInitialized = true;
 
-
-            if (appInitialized)
-            {
-
+            if (appInitialized) {
                 can_send_taskData.state = CAN_SEND_TASK_STATE_SERVICE_TASKS;
             }
             break;
         }
 
-        case CAN_SEND_TASK_STATE_SERVICE_TASKS:
-        {
-          //  printf("\n\rCAN task\n\r");
-            
-            
-            xQueueReceive(Bat_Voltage_Queue,&voltage,pdMS_TO_TICKS(300));
-            xQueueReceive(Temperature_Queue,&temp,portMAX_DELAY);
-            temp = 17;
-            //voltage = 240;
-            printf("\n\n\n\r Queue temp : %f \r\n\n\n\r",temp);
-            int voltage_int = voltage*10;
-            uint8_t MSB_voltage = (voltage_int >> 8) & 0xF;
-            uint8_t LSB_voltage = (voltage_int << 8) & 0xF;
-            int temp_int = temp*10;
-            uint8_t MSB_temp = (temp_int >> 8) & 0xF;
-            uint8_t LSB_temp = (temp_int << 8) & 0xF;
-            LED_RB13_Toggle();
-            printf("\n\n\rMSD : %d  \tLSB: %d\n\n\n\\r",MSB_temp,LSB_temp);
-            message[0] = MSB_voltage;
-            message[1] = LSB_voltage;
-            message[2] = MSB_temp;
-            message[3] = LSB_temp;
-            id = 0x54;
-            
-           // xSemaphoreTake(CAN_Mutex, portMAX_DELAY);
-            //{
-                CanSend_task(id, length, message);
-            //}
-           // xSemaphoreGive(CAN_Mutex);
-            //*** Data to send to can ***//
-            //Low Voltage battery
-            //Ignition            uint8_t LSB_temp = (temp_int << 8) & 0xF;
+        case CAN_SEND_TASK_STATE_SERVICE_TASKS: {
+            //  printf("\n\rCAN task\n\r");
 
-            //VCU state
-            //Temperatures
-            //Fan control
-            //**************************//         
+            xQueueReceive(Bat_Voltage_Queue, &voltage, pdMS_TO_TICKS(300));
+            xQueueReceive(Temperature_Queue, &temp, portMAX_DELAY);
+            temp = 17;
+            // voltage = 240;
+            printf("\n\n\n\r Queue temp : %f \r\n\n\n\r", temp);
+            int voltage_int = voltage * 10;
+
+            int temp_int = temp * 10;
+
+            LED_RB13_Toggle();
+            printf("\n\n\rMSD : %d  \tLSB: %d\n\n\n\\r", MSB_temp, LSB_temp);
+            message[0] = voltage_int & 0xFF;
+            message[1] = (voltage_int >> 8) & 0xFF;
+            message[2] = temp_int & 0xFF;
+            message[3] = (temp_int >> 8) & 0xFF;
+            id = 0x54;
+
+            // xSemaphoreTake(CAN_Mutex, portMAX_DELAY);
+            //{
+            CanSend_task(id, length, message);
+            //}
+            // xSemaphoreGive(CAN_Mutex);
+            //*** Data to send to can ***//
+            // Low Voltage battery
+            // Ignition            uint8_t LSB_temp = (temp_int << 8) & 0xF;
+
+            // VCU state
+            // Temperatures
+            // Fan control
+            //**************************//
             break;
-            
         }
 
         /* TODO: implement your application state machine.*/
 
-
         /* The default state should never be executed. */
-        default:
-        {
+        default: {
             /* TODO: Handle error in application's state machine. */
             break;
         }
     }
 }
-
 
 /*******************************************************************************
  End of File
