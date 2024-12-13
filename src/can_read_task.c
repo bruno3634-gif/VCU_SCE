25040387f3810684383e9fbd6b247b68960968bd
bruno vicente <bruno.marvicente@gmail.com>
2024-12-13 22:15:29 +0000

    s emergency working

--- src/can_read_task.c
+++ src/can_read_task.c
@@ -97,7 +97,6 @@ void task_function(){
     uint8_t rx_message[8];
 
 
-xSemaphoreTake(CAN_Mutex, portMAX_DELAY);
     if (CAN1_MessageReceive(&can_read_taskData.id, &lenght, rx_message, 0, 2, &msgAttr) == true) {
         //CAN1_MessageTransmit(0x200,lenght,can_read_taskData.rx_message,0, CANFD_MODE_NORMAL, CANFD_MSG_TX_DATA_FRAME);
         switch (can_read_taskData.id) {
@@ -116,7 +115,7 @@ xSemaphoreTake(CAN_Mutex, portMAX_DELAY);
     } else {
         //taskYIELD();
     }
-    xSemaphoreGive(CAN_Mutex);
+
 }
 
 
